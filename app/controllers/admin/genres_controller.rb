43aaae11ecb5541850d4bb947f67c8c52be10e8e
yukari-ishii <115003222+yukari-ishii@users.noreply.github.com>
2022-11-26 15:09:42 +0000

    [Finish] ジャンル一覧・編集画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,33 @@
 class Admin::GenresController < ApplicationController
+
+  def index
+    #ジャンル新規登録のための変数
+    @genre = Genre.new
+    #ジャンル一覧表示のための変数
+    @genres = Genre.all
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
+  end
+
+  private
+
+  #ストロングパラメータ
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,36 @@
 class Admin::ItemsController < ApplicationController
+  
+  def index
+    @items = Item.all
+  end
+  
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
+  end
+
+  def show
+  
+  end
+  
+  def edit
+  
+  end
+  
+  def update
+  
+  end
+
+  private
+
+  #ストロングパラメータ
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :is_active)
+  end
+
 end
